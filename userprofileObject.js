58ede0eff24644773157508790278a09e951cc98
bhlezzdmplg <yharrasallyguiao@gmail.com>
2024-11-08 21:47:26 +0800

    Update userprofileObject.js

--- userprofileObject.js
+++ userprofileObject.js
@@ -1,20 +1,8 @@
-let userProfile = {
-  fullName: prompt("Enter your full name: "),
-  age: parseInt(prompt("Enter your age: ")),
-  favoriteNumber: parseInt(prompt("Enter your favorite number: ")),
-  favoriteColors: []
-};
-
-let keepGoing = true;
-
-while (keepGoing) {
-  let color = prompt("Enter a favorite color (or type 'done' to finish): ");
-
-  if (color.toLowerCase() === 'done') {
-    keepGoing = false;
-  } else {
-    userProfile.favoriteColors.push(color);
-  }
-}
-
-console.log(userProfile);
+const userProfile = {
+    fullName: "Bhlezz Garbin",
+    age: 19,
+    favoriteNumber: 24,
+    favoriteColors: ["purple", "lavender", "white"]
+  };
+  
+  console.log(userProfile);
